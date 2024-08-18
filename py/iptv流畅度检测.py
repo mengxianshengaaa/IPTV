9b3683c7cb2be8248b1203d75261dc0be83b1019
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-08-18 12:41:31 +0800

    Update iptv流畅度检测.py

--- py/iptv流畅度检测.py
+++ py/iptv流畅度检测.py
@@ -584,23 +584,20 @@ check_and_write_file('2.txt',  'a1.txt',  keywords="央视频道, 剧场, 电影
 check_and_write_file('2.txt',  'b.txt',  keywords="卫视频道, 卫视, 凤凰, 星空")
 check_and_write_file('2.txt',  'c.txt',  keywords="影视频道, 爱动漫, SiTV, 爱怀旧, 爱经典, 爱科幻, 爱青春, 爱悬疑, 爱幼教, 爱院线, 影, 剧, 经典")
 check_and_write_file('2.txt',  'd.txt',  keywords="少儿频道, 少儿, 卡通, 动漫, 宝贝, 哈哈")
-check_and_write_file('2.txt',  'e.txt',  keywords="福建频道, 福建")
-#check_and_write_file('2.txt',  'f.txt',  keywords="湖南频道, 湖南")
+check_and_write_file('2.txt',  'e.txt',  keywords="河南频道, 河南")
+check_and_write_file('2.txt',  'f0.txt',  keywords="河北频道, 石家庄, 邢台")
+check_and_write_file('2.txt',  'f.txt',  keywords="河北频道, 河北")
 check_and_write_file('2.txt',  'g.txt',  keywords="广东频道, 广东")
 check_and_write_file('2.txt',  'h.txt',  keywords="广西频道, 广西")
-check_and_write_file('2.txt',  'i.txt',  keywords="河南频道, 河南")
-check_and_write_file('2.txt',  'j0.txt',  keywords="河北频道, 石家庄, 邢台")
-check_and_write_file('2.txt',  'j.txt',  keywords="河北频道, 河北")
-check_and_write_file('2.txt',  'k.txt',  keywords="北京频道, 北京")
-#check_and_write_file('2.txt',  'l.txt',  keywords="山东频道, 山东")
-check_and_write_file('2.txt',  'm.txt',  keywords="浙江频道, 浙江")
-#check_and_write_file('2.txt',  'n.txt',  keywords="重庆频道, 重庆")
-check_and_write_file('2.txt',  'o.txt',  keywords="江苏频道, 江苏")
-check_and_write_file('2.txt',  'p.txt',  keywords="陕西频道, 陕西")
+check_and_write_file('2.txt',  'i.txt',  keywords="北京频道, 北京")
+check_and_write_file('2.txt',  'j.txt',  keywords="浙江频道, 浙江")
+check_and_write_file('2.txt',  'k.txt',  keywords="江苏湖南, 江苏, 湖南")
+check_and_write_file('2.txt',  'l.txt',  keywords="陕西频道, 陕西")
+check_and_write_file('2.txt',  'm.txt',  keywords="福建频道, 福建")
 ###############################################################################################################################################################################################################################
 ##############################################################对生成的文件进行合并
 file_contents = []
-file_paths = ["a0.txt", "a.txt", "a1.txt", "b.txt", "c.txt", "d.txt", "j0.txt", "j.txt", "i.txt", "e.txt", "f.txt", "g.txt", "h.txt", "k.txt", "l.txt", "m.txt", "n.txt","o.txt", "p.txt"]  # 替换为实际的文件路径列表
+file_paths = ["a0.txt", "a.txt", "a1.txt", "b.txt", "c.txt", "d.txt", "e.txt", "f0.txt", "f.txt", "g.txt", "h.txt", "i.txt", "j.txt", "k.txt", "l.txt", "m.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding="utf-8") as file:
@@ -665,8 +662,8 @@ with open('综合源.txt', 'a', encoding="utf-8") as file:
 
 
 ################################################################################################任务结束,删除不必要的过程文件
-files_to_remove = ['去重.txt', '分类.txt', "2.txt", "4.txt", "5.txt", "playlist/3.txt", "a0.txt", "a.txt", "a1.txt", "b.txt", \
-                   "c.txt", "d.txt", "e.txt", "f.txt", "g.txt", "h.txt",  "i.txt", "j0.txt", "j.txt", "k.txt", "l.txt", "m.txt", "n.txt","o.txt", "p.txt"]
+files_to_remove = ['去重.txt', '分类.txt', "2.txt", "4.txt", "5.txt", "a0.txt", "a.txt", "a1.txt", "b.txt", "c.txt", "d.txt", \
+                   "e.txt", "f0.txt", "f.txt", "g.txt", "h.txt", "i.txt", "j.txt", "k.txt", "l.txt", "m.txt"]
 for file in files_to_remove:
     if os.path.exists(file):
         os.remove(file)
