7c181721a6bafe231f1cd82e5e4732f7c7644005
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-08-26 00:40:28 +0800

    Update 一锅烩.py

--- py/一锅烩.py
+++ py/一锅烩.py
@@ -221,32 +221,13 @@ replacements = {
 with open('汇总.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
 # 创建新文件并写入替换后的内容
-with open('汇总.txt', 'w', encoding='utf-8') as new_file:
+with open('2.txt', 'w', encoding='utf-8') as new_file:
     for line in lines:
         for old, new in replacements.items():
             line = line.replace(old, new)
         new_file.write(line)
 print("替换完成，新文件已保存。")
 
-
-
-
-import re
-from pypinyin import lazy_pinyin
-# 打开一个utf-8编码的文本文件
-with open("汇总.txt", "r", encoding="utf-8") as file:
-    # 读取所有行并存储到列表中
-    lines = file.readlines()
-# 定义一个函数，用于提取每行的第一个数字
-def extract_first_number(line):
-    match = re.search(r'\d+', line)
-    return int(match.group()) if match else float('inf')
-# 对列表中的行进行排序，按照第一个数字的大小排列，其余行按中文排序
-sorted_lines = sorted(lines, key=lambda x: (not 'CCTV' in x, extract_first_number(x) if 'CCTV' in x else lazy_pinyin(x.strip())))
-# 将排序后的行写入新的utf-8编码的文本文件
-with open("2.txt", "w", encoding="utf-8") as file:
-    for line in sorted_lines:
-        file.write(line)
 
 
 import re
@@ -362,6 +343,22 @@ with open('2.txt', 'w', encoding='utf-8') as new_file:
         new_file.write(line)  # 写入新文件
 print("新文件已保存。")  # 打印完成信息
 
+import re
+from pypinyin import lazy_pinyin
+# 打开一个utf-8编码的文本文件
+with open("2.txt", "r", encoding="utf-8") as file:
+    # 读取所有行并存储到列表中
+    lines = file.readlines()
+# 定义一个函数，用于提取每行的第一个数字
+def extract_first_number(line):
+    match = re.search(r'\d+', line)
+    return int(match.group()) if match else float('inf')
+# 对列表中的行进行排序，按照第一个数字的大小排列，其余行按中文排序
+sorted_lines = sorted(lines, key=lambda x: (not 'CCTV' in x, extract_first_number(x) if 'CCTV' in x else lazy_pinyin(x.strip())))
+# 将排序后的行写入新的utf-8编码的文本文件
+with open("2.txt", "w", encoding="utf-8") as file:
+    for line in sorted_lines:
+        file.write(line)
 
 
 ########################################################################################################################################################################################
