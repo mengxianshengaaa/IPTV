e03c92c715b4cdb650b05ecbd49c81c8a35f1e6f
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-09-02 17:43:38 +0800

    Update 酒店源.py

--- py/酒店源.py
+++ py/酒店源.py
@@ -415,7 +415,6 @@ for url in urls:
                         name = item.get('name')
                         # 从字典中获取'url'键的值，如果键不存在则返回None
                         urlx = item.get('url')
-                        print(f"过滤要排除的关键词行: {urlx}")
                         match = re.search(r'182\.117\.136\.\d{1,3}\.\d{1,3}\:\d{1,5}|192\.168\.\d{1,3}\.\d{1,3}\:\d{1,5}|10\.0\.0\.\d{1,3}\:\d{1,5}', urlx)
                         # 如果urlx包含'udp'或'rtp'字符串，则跳过当前循环的剩余部分
                         if 'udp' in urlx or 'rtp' in urlx:
