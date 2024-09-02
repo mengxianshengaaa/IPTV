e4014d69a39c1e64f1b7ad23be75d464b0173f3a
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-09-02 18:47:03 +0800

    Update 酒店源.py

--- py/酒店源.py
+++ py/酒店源.py
@@ -415,13 +415,8 @@ for url in urls:
                         name = item.get('name')
                         # 从字典中获取'url'键的值，如果键不存在则返回None
                         urlx = item.get('url')
-                        match = re.search(r'182\.117\.136\.\d{1,3}\:\d{1,5}|192\.168\.1\.\d{1,3}\:\d{1,5}|10\.0\.0\.\d{1,3}\:\d{1,5}', urlx)
-                       # 使用正则表达式判断 IP
-                        if match:
-                            continue                 
-                            print(f"要排除的ip行: {match.group()}")
                         # 如果urlx包含'udp'或'rtp'字符串，则跳过当前循环的剩余部分
-                        if 'udp' in urlx or 'rtp' in urlx:
+                        if 'udp' in urlx or 'rtp' in urlx or '182.117' in urlx:
                             continue  # 跳过包含'udp'或'rtp'的url
                         # 如果urlx以'http'开头，则直接使用这个url
                         if 'http' in urlx:
