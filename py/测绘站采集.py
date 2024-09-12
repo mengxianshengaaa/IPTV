b4e3b5df95ca8297f03d8903ac48cff578ef7fae
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-09-12 22:30:23 +0800

    Update 测绘站采集.py

--- py/测绘站采集.py
+++ py/测绘站采集.py
@@ -443,10 +443,10 @@ def txt_to_m3u(input_file, output_file):
     current_time = now.strftime("%m-%d %H:%M")
     with open(output_file, 'w', encoding='utf-8') as f:  
         f.write('#EXTM3U x-tvg-url="https://live.fanmingming.com/e.xml" catchup="append" catchup-source="?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"\n')
-        f.write(f'#EXTINF:-1 group-title="更新时间",请您欣赏\n')    
-        f.write(f'https://vd2.bdstatic.com/mda-nk3am8nwdgqfy6nh/sc/cae_h264/1667555203921394810/mda-nk3am8nwdgqfy6nh.mp4\n')    
-        f.write(f'#EXTINF:-1 group-title="{current_time}",虚情的爱\n')    
-        f.write(f'https://vd2.bdstatic.com/mda-mi1dd05gmhwejdwn/sc/cae_h264/1630576203346678103/mda-mi1dd05gmhwejdwn.mp4\n')    
+        #f.write(f'#EXTINF:-1 group-title="更新时间",请您欣赏\n')    
+        #f.write(f'https://vd2.bdstatic.com/mda-nk3am8nwdgqfy6nh/sc/cae_h264/1667555203921394810/mda-nk3am8nwdgqfy6nh.mp4\n')    
+        #f.write(f'#EXTINF:-1 group-title="{current_time}",虚情的爱\n')    
+        #f.write(f'https://vd2.bdstatic.com/mda-mi1dd05gmhwejdwn/sc/cae_h264/1630576203346678103/mda-mi1dd05gmhwejdwn.mp4\n')    
         # 初始化genre变量
         genre = ''
         # 遍历txt文件内容
@@ -463,9 +463,23 @@ def txt_to_m3u(input_file, output_file):
                     # 将频道信息写入m3u文件
                     f.write(f'#EXTINF:-1 tvg-logo="https://live.fanmingming.com/tv/{channel_name}.png" group-title="{genre}",{channel_name}\n')
                     f.write(f'{channel_url}\n')
-        
 # 将txt文件转换为m3u文件
 txt_to_m3u('综合源.txt', '综合源.m3u')
+
+
+import datetime
+now = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+current_time = now.strftime("%m-%d %H:%M")
+# 打开文本文件并将时间添加到开头
+file_path = "综合源.m3u"
+with open(file_path, 'r+', encoding='utf-8') as f:
+    content = f.read()
+    f.seek(0, 0)
+    f.write(f'{content}\n')
+    f.write(f'#EXTINF:-1 group-title="更新时间",请您欣赏\n')    
+    f.write(f'http://em.21dtv.com/songs/60144971.mkv\n')    
+    f.write(f'#EXTINF:-1 group-title="{current_time}",虚情的爱\n')    
+    f.write(f'https://vd2.bdstatic.com/mda-mi1dd05gmhwejdwn/sc/cae_h264/1630576203346678103/mda-mi1dd05gmhwejdwn.mp4\n')       
 
 #任务结束,删除不必要的过程文件#
 files_to_remove = ['组播源.txt', "TW.txt", "a.txt", "主.txt", "b.txt", "b1.txt", "港澳.txt", "省市.txt", "df.txt", "df1.txt", "少儿1.txt", "sr2.txt", \
