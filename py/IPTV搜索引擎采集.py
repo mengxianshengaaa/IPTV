44b273d0b8cb2774a89b83ac22b80fde1aa3846c
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-09-12 17:39:27 +0800

    Update IPTV搜索引擎采集.py

--- py/IPTV搜索引擎采集.py
+++ py/IPTV搜索引擎采集.py
@@ -417,7 +417,7 @@ def txt_to_m3u(input_file, output_file):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")  
     with open(output_file, 'w', encoding='utf-8') as f:
-        f.write(f'#EXTINF:-1 ,更新时间{current_time}\n')    
+        f.write(f'#EXTINF:-1 group-title="更新时间{current_time}",请您欣赏\n')    
         f.write(f'https://vd2.bdstatic.com/mda-mi1dd05gmhwejdwn/sc/cae_h264/1630576203346678103/mda-mi1dd05gmhwejdwn.mp4\n')    
         f.write('#EXTM3U x-tvg-url="https://live.fanmingming.com/e.xml" catchup="append" catchup-source="?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"\n')
         # 初始化genre变量
