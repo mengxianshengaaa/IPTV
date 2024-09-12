b9a9477df14fb16cf72e817ed1e2efca45c1797c
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-09-12 21:11:04 +0800

    Update 测绘站采集.py

--- py/测绘站采集.py
+++ py/测绘站采集.py
@@ -757,11 +757,11 @@ file_path = "综合源.txt"
 with open(file_path, 'r+', encoding='utf-8') as f:
     content = f.read()
     f.seek(0, 0)
-    f.write(f'{content\n')
-    f.write(f'{更新时间,#genre#\n')
+    f.write(f'{content}\n')
+    f.write(f'更新时间,#genre#\n')
     f.write(f'请您欣赏,https://vd2.bdstatic.com/mda-mi1dd05gmhwejdwn/sc/cae_h264/1630576203346678103/mda-mi1dd05gmhwejdwn.mp4\n')
     f.write(f'{current_time},#genre#\n')
-    f.write(f'虚情的爱,https://vd2.bdstatic.com/mda-mi1dd05gmhwejdwn/sc/cae_h264/1630576203346678103/mda-mi1dd05gmhwejdwn.mp4\n}')
+    f.write(f'虚情的爱,https://vd2.bdstatic.com/mda-mi1dd05gmhwejdwn/sc/cae_h264/1630576203346678103/mda-mi1dd05gmhwejdwn.mp4\n')
 
 
 ################################################################################################任务结束,删除不必要的过程文件
