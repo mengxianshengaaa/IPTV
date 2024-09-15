c1e8d8ada801a6ee13072b3a15789e9637ae8e3a
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-09-15 13:45:23 +0800

    Update 更新文件名.py

--- py/更新文件名.py
+++ py/更新文件名.py
@@ -1,23 +1,28 @@
+import datetime
 import os
 
-# 要重命名的文件名列表（初始文件名）
-initial_filenames = ['综合源.m3u', '组播优选.txt', '综合源.txt']
+# 获取当前日期时间
+now = datetime.datetime.now()
+current_date = now.strftime("%m%d")
+
+# 要重命名的纯中文文件名列表
+chinese_filenames = ['综合源.m3u', '组播优选.txt', '综合源.txt']
 
 # 获取当前目录下的所有文件
 all_files = os.listdir(os.getcwd())
 
-# 第一步：删除以数字加中文格式命名的 txt 和 m3u 文件
+# 删除以数字加中文形式命名的文件
 for old_filename in all_files:
-    if (old_filename.endswith('.txt') or old_filename.endswith('.m3u')) and any(char.isdigit() for char in old_filename):
+    if any(char.isdigit() for char in old_filename) and (old_filename.endswith('.txt') or old_filename.endswith('.m3u')):
         full_old_path = os.path.join(os.getcwd(), old_filename)
         if os.path.exists(full_old_path):
             os.remove(full_old_path)
             print(f"Deleted {old_filename}")
 
-# 第二步：重命名初始文件名的文件
+# 重命名纯中文文件名的文件
 for old_filename in all_files:
-    if any(old_filename.endswith(init_filename) for init_filename in initial_filenames):
-        new_filename = f"{old_filename[:-len(old_filename.split('.')[-1])]}_新命名{old_filename.split('.')[-1]}"
+    if any(old_filename.endswith(init_filename) for init_filename in chinese_filenames):
+        new_filename = f"{current_date}{old_filename}"
         full_old_path = os.path.join(os.getcwd(), old_filename)
         full_new_path = os.path.join(os.getcwd(), new_filename)
         os.rename(full_old_path, full_new_path)
